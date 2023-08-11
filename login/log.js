b9b301cfa3ab97487a6d900b5d0424f718996fb4
oswin <oswinalex1@gmail.com>
2023-08-12 01:21:38 +0530

    login page

---
+++ login/log.js
@@ -0,0 +1,14 @@
+function replace(){
+    document.getElementById("button")
+    if ("button"){
+         
+        document.getElementById("log").style.display="inline-block";
+        document.getElementById("log2").style.display="none";
+        
+}
+   else{
+  
+    document.getElementById("log2").style.display="inline-block";
+    document.getElementById("log").style.display="none";
+   }
+}
