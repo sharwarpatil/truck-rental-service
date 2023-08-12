8cf99192b6a85b10b0b6e4d2b6ca33bd0dcf23ca
oswin <oswinalex1@gmail.com>
2023-08-13 00:01:09 +0530

    login_page

--- login/log.js
+++ login/log.js
@@ -1,7 +1,7 @@
 function replace(){
     document.getElementById("button")
     if ("button"){
-         
+// if sign-in putton is pressed the second div dissapers and the first div is displayed on screen 
         document.getElementById("log").style.display="inline-block";
         document.getElementById("log2").style.display="none";
         
@@ -11,4 +11,22 @@ function replace(){
     document.getElementById("log2").style.display="inline-block";
     document.getElementById("log").style.display="none";
    }
+}
+
+
+function back_to_sign_In(){
+
+    document.getElementById("signup")
+
+    if("signup"){
+        
+        document.getElementById("log2").style.display="inline-block";
+        document.getElementById("log").style.display="none";
+        
+}
+   else{
+  
+    document.getElementById("log").style.display="inline-block";
+    document.getElementById("log2").style.display="none";
+    }
 }
