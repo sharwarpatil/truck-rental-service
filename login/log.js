f48b06f692266e6cd6589d72dd43a9c0fb33c860
oswin <oswinalex1@gmail.com>
2023-08-30 03:00:21 +0530

    log-in page

--- login/log.js
+++ login/log.js
@@ -1,61 +1,23 @@
-let replace=()=>{
-    document.getElementById("button")
-    if ("button"){
-// if sign-in putton is pressed the second div dissapers and the first div is displayed on screen 
-        document.getElementById("log").style.display="inline-block";
-        document.getElementById("log2").style.display="none";
-        
-}
-   else{
-  
-    document.getElementById("log2").style.display="inline-block";
-    document.getElementById("log").style.display="none";
-   }
-}
-
-
-function back_to_sign_In(){
-
-    document.getElementById("signup")
-
-    if("signup"){
-        
-        document.getElementById("log2").style.display="inline-block";
-        document.getElementById("log").style.display="none";
-        
-}
-   else{
-  
-    document.getElementById("log").style.display="inline-block";
-    document.getElementById("log2").style.display="none";
-
+let replaceit = () => {
+  let loginElement = document.getElementById("login");
+  let box1Element = document.getElementById("box1");
+  let box2Element = document.getElementById("box2");
+  let signup = document.getElementById("signup");
+  if (loginElement) {
+      box1Element.style.display = "none";
+      box2Element.style.display = "inline";
+  } else {
+      box2Element.style.display = "none";
+      box1Element.style.display = "inline";
+  }
+ 
+  if (signup) {
+    box1Element.style.display = "inline";
+    box2Element.style.display = "none";
+} else {
+    box2Element.style.display = "inline";
+    box1Element.style.display = "none";
 }
 
-}
-
-
-let form1 =()=>{
-    let name = Document.form["sign-up"]["user_name"].value
-    let email =Document.form["sign-up"]["email"].value
-    let password =Document.form["sign-up"]["passwprd"].value
-    let rpassword =Document.form["sign-up"]["re-password"].value
-
-    if(name==""||email =="" ||password==""||rpassword ==""){
-        alert("Enter all your details")
-    }
-    else{
-        alert("sucessfull sign-up")
-    }
-}
-let form2 =()=>{
-    let username22= Document.forms["login"]["user-name2"].value
-    let password22 = Document.forms["login"]["password3"].value
-
-    if(username22==""||password22==""){
-        alert("enter all your details")
-    }
-    else{
-        alert('login sucessfully')
-    }
 
 }
