c8d799b2644159444d6544efc66e2aef9e9bdd18
oswin <oswinalex1@gmail.com>
2023-09-16 20:25:28 +0530

    switch

--- r_app/src/App.js
+++ r_app/src/App.js
@@ -47,6 +47,33 @@ setInterval(() => {
 }, 12000); 
 
 
+
+// for button
+
+
+let btn_on= () => {
+  let box1= document.getElementById("btn_radio")
+ let box2 = document.getElementById("btn_radio2")
+ box1.style.display ="none"
+ box2.style.display="block"
+ 
+
+}
+let btn_of= () => {
+  let box1= document.getElementById("btn_radio")
+ let box2 = document.getElementById("btn_radio2")
+ box2.style.display ="none"
+ box1.style.display="block"
+ 
+
+}
+
+
+
+
+
+
+
 function App() {
   return (
     <>
@@ -76,17 +103,49 @@ function App() {
         
          
         <div id="under_image_text">
-         <p>
+     
+         <p id="text">
+          
              Rent your truck Now
+
+          <i class="bi bi-truck"></i>
+          <svg xmlns="http://www.w3.org/2000/svg" id="icon" fill="currentColor" className="bi bi-truck" viewBox="0 0 16 16">
+          <path d="M0 3.5A1.5 1.5 0 0 1 1.5 2h9A1.5 1.5 0 0 1 12 3.5V5h1.02a1.5 1.5 0 0 1 1.17.563l1.481 1.85a1.5 1.5 0 0 1 .329.938V10.5a1.5 1.5 0 0 1-1.5 1.5H14a2 2 0 1 1-4 0H5a2 2 0 1 1-3.998-.085A1.5 1.5 0 0 1 0 10.5v-7zm1.294 7.456A1.999 1.999 0 0 1 4.732 11h5.536a2.01 2.01 0 0 1 .732-.732V3.5a.5.5 0 0 0-.5-.5h-9a.5.5 0 0 0-.5.5v7a.5.5 0 0 0 .294.456zM12 10a2 2 0 0 1 1.732 1h.768a.5.5 0 0 0 .5-.5V8.35a.5.5 0 0 0-.11-.312l-1.48-1.85A.5.5 0 0 0 13.02 6H12v4zm-9 1a1 1 0 1 0 0 2 1 1 0 0 0 0-2zm9 0a1 1 0 1 0 0 2 1 1 0 0 0 0-2z"/>
+          </svg>
+
+
+          <i class="bi bi-truck"></i>
+          <svg xmlns="http://www.w3.org/2000/svg" id="icon" fill="currentColor" className="bi bi-truck" viewBox="0 0 16 16">
+          <path d="M0 3.5A1.5 1.5 0 0 1 1.5 2h9A1.5 1.5 0 0 1 12 3.5V5h1.02a1.5 1.5 0 0 1 1.17.563l1.481 1.85a1.5 1.5 0 0 1 .329.938V10.5a1.5 1.5 0 0 1-1.5 1.5H14a2 2 0 1 1-4 0H5a2 2 0 1 1-3.998-.085A1.5 1.5 0 0 1 0 10.5v-7zm1.294 7.456A1.999 1.999 0 0 1 4.732 11h5.536a2.01 2.01 0 0 1 .732-.732V3.5a.5.5 0 0 0-.5-.5h-9a.5.5 0 0 0-.5.5v7a.5.5 0 0 0 .294.456zM12 10a2 2 0 0 1 1.732 1h.768a.5.5 0 0 0 .5-.5V8.35a.5.5 0 0 0-.11-.312l-1.48-1.85A.5.5 0 0 0 13.02 6H12v4zm-9 1a1 1 0 1 0 0 2 1 1 0 0 0 0-2zm9 0a1 1 0 1 0 0 2 1 1 0 0 0 0-2z"/>
+          </svg>
+
+
+          
+
          </p>
 
         </div>
 
 
         <div id="beside_card">
-          <p>
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eum beatae voluptatibus consectetur amet ducimus laboriosam illo molestiae aliquid labore itaque! Ad placeat laudantium incidunt rerum eveniet, omnis dolores consequatur ratione?
-          </p>
+
+          <div onclick="btn_on()" id="btn_radio">
+          <div id="circle_btn1"></div>
+
+      
+
+          </div>
+
+          <div onclick="btn_of()" id="btn_radio2">
+          
+      
+          <div id="circle_btn2"></div>
+          
+      
+          </div>
+    
+    
+          
         </div>
 
         <div id="content"   >
