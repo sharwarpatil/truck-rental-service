193c95f810b6d915ab40ad3127f6f7ad0d0101ee
oswin <oswinalex1@gmail.com>
2023-09-23 13:42:40 +0530

    buttons and dropdown

--- r_app/src/component/card.js
+++ r_app/src/component/card.js
@@ -16,15 +16,7 @@ export default function Card(props) {
                 <div className="card-body">
                   <div id="cardtext"></div>
                   <p className="card-text"> 
-                    Small tempo 
-                    model:35353 
-                    capacity :400kg
-                    cost :2000 rs
-                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. 
-                    Natus culpa illo tempore repellat ut incidunt illum laborum! 
-                    Saepe, voluptates minima. Assumenda quam illo ipsam iusto minima
-                     labore repellendus exercitationem tempore.
-
+                    {props.text}
               
 
                   </p>
