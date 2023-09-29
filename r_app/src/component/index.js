cc43b2c1827bd33476fc9dc9c1b773b9241b83f1
oswin <oswinalex1@gmail.com>
2023-09-29 21:17:49 +0530

    new slider component

---
+++ r_app/src/component/index.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import image1 from './images/t1.jpeg';
+import image2 from './images/t2.jpeg';
+import image3 from './images/t3.jpeg';
+
+
+
+
+
+function Index(){
+  return (
+    <div className='container'>
+      <div id="carouselExampleIndicators" className="carousel slide" data-ride="carousel">
+        <ol className="carousel-indicators">
+          <li data-target="#carouselExampleIndicators" data-slide-to="0" className="active"></li>
+          <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+          <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+        </ol>
+        <div className="carousel-inner">
+          <div className="carousel-item active">
+            <img className="d-block w-100" src={image1} alt="Image 1" />
+          </div>
+          <div className="carousel-item">
+            <img className="d-block w-100" src={image2} alt="Image 2" />
+          </div>
+          <div className="carousel-item">
+            <img className="d-block w-100" src={image3} alt="Image 3" />
+          </div>
+        </div>
+        <a className="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+          <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+          <span className="sr-only">Previous</span>
+        </a>
+        <a className="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+          <span className="carousel-control-next-icon" aria-hidden="true"></span>
+          <span className="sr-only">Next</span>
+        </a>
+      </div>
+    </div>
+  );
+}
+
+
+export default Index;
